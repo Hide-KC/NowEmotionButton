c34e3ea7176da7b73d8156d1144ba2568d982749
Hide-KC <kcs.dev.labo@gmail.com>
2020-07-02 20:44:44 +0900

    init

---
+++ app/src/main/java/work/kcs_labo/now_emotion_button/MainActivity.kt
@@ -0,0 +1,12 @@
+package work.kcs_labo.now_emotion_button
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}

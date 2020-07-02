3337307876d3b51a443f87015ff87a898b7a0576
Hide-KC <kcs.dev.labo@gmail.com>
2020-07-03 00:00:34 +0900

    init Firebase, AdMob sample code

--- app/src/main/java/work/kcs_labo/now_emotion_button/MainActivity.kt
+++ app/src/main/java/work/kcs_labo/now_emotion_button/MainActivity.kt
@@ -2,11 +2,21 @@ package work.kcs_labo.now_emotion_button
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    MobileAds.initialize(this) { _ ->
+      //TODO("Not yet implemented")
+      println("MobileAds initialized.")
+    }
+
+    banner_ads.loadAd(AdRequest.Builder().build())
   }
 }

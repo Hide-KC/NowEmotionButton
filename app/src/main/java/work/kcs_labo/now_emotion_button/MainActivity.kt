62208946cbd1ab1ad02e0e02f1e589e64b5dc15f
Hide-KC <kcs.dev.labo@gmail.com>
2020-07-05 20:14:28 +0900

    Design MainActivity layout

--- app/src/main/java/work/kcs_labo/now_emotion_button/MainActivity.kt
+++ app/src/main/java/work/kcs_labo/now_emotion_button/MainActivity.kt
@@ -2,21 +2,50 @@ package work.kcs_labo.now_emotion_button
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.MobileAds
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.main_activity.*
+import work.kcs_labo.now_emotion_button.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+
+    setupWidget(binding)
 
     MobileAds.initialize(this) { _ ->
       //TODO("Not yet implemented")
       println("MobileAds initialized.")
     }
+    bannerAds.loadAd(AdRequest.Builder().build())
+  }
 
-    banner_ads.loadAd(AdRequest.Builder().build())
+  private fun setupWidget(binding: MainActivityBinding) {
+    binding.bottomNavigation.setOnNavigationItemSelectedListener { item ->
+      when (item.itemId) {
+        R.id.home -> {
+          //TODO("Not yet implemented")
+          return@setOnNavigationItemSelectedListener true
+        }
+        R.id.people -> {
+          //TODO("Not yet implemented")
+          return@setOnNavigationItemSelectedListener true
+        }
+        R.id.calendar -> {
+          //TODO("Not yet implemented")
+          return@setOnNavigationItemSelectedListener true
+        }
+        R.id.setting -> {
+          //TODO("Not yet implemented")
+          return@setOnNavigationItemSelectedListener true
+        }
+        else -> {
+          throw IllegalArgumentException("Illegal itemId was selected")
+        }
+      }
+    }
   }
 }
---
+++ app/src/main/java/work/kcs_labo/now_emotion_button/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package work.kcs_labo.now_emotion_button
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainActivityViewModel(app: Application) : AndroidViewModel(app) {
+}
